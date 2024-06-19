dbc52fa19d9287dcdb1c50f33b34a1ce27173431
so-zero <zero_s0@naver.com>
2024-06-19 21:38:11 +0900

    React-Router-Dom 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return <h1 className="text-3xl text-red-500">App</h1>;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Home from "./pages/Home.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Posts from "./pages/Posts.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import Search from "./pages/Search.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/posts", element: <Posts /> },
+    ],
+  },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/search", element: <Search /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
