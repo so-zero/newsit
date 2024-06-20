e4445f4e5fad932e13076177c9d8282248ac4242
so-zero <zero_s0@naver.com>
2024-06-20 21:17:24 +0900

    login page 로그인 기능 추가

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,57 @@
-import { TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { TextInput, Alert, Spinner } from "flowbite-react";
+import axios from "axios";
 
 export default function Login() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setData({ ...data, [name]: value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    const URL = `${import.meta.env.VITE_BACKEND_URL}/auth/login`;
+
+    try {
+      const response = await axios.post(URL, data);
+      await response.data;
+      navigate("/");
+      setLoading(false);
+    } catch (error) {
+      setError(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="mt-20 px-3 flex flex-col max-w-screen-sm mx-auto">
       <h1 className="font-title uppercase text-5xl text-center">Newsit</h1>
-      <form className="flex flex-col gap-4 mt-10 px-10">
+      {error && (
+        <Alert className="mt-10 mx-10" color="failure">
+          {error}
+        </Alert>
+      )}
+      <form className="flex flex-col gap-4 mt-10 px-10" onSubmit={handleSubmit}>
         <div>
           <TextInput
             type="text"
             placeholder="이메일"
-            id="email"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
             color="base"
             sizing="lg"
             required
@@ -21,14 +61,27 @@ export default function Login() {
           <TextInput
             type="password"
             placeholder="비밀번호"
-            id="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
             color="base"
             sizing="lg"
             required
           />
         </div>
-        <button className="my-5 bg-black text-white w-full py-3 rounded-md transition hover:-translate-y-2 hover:shadow-[5px_5px_0px_0px_rgba(100,100,100)]">
-          로그인
+        <button
+          type="submit"
+          disabled={loading}
+          className="my-5 bg-black text-white w-full py-3 rounded-md transition hover:-translate-y-2 hover:shadow-[5px_5px_0px_0px_rgba(100,100,100)]"
+        >
+          {loading ? (
+            <>
+              <Spinner size="sm" />
+              <span className="pl-3">로딩중...</span>
+            </>
+          ) : (
+            "로그인"
+          )}
         </button>
       </form>
       <Link
