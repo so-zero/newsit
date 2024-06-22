b9a7ce296620af6b0f3c7fb329d2fcf6861cb85d
so-zero <zero_s0@naver.com>
2024-06-22 12:35:11 +0900

    header currentUser 기능 추가

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Navbar } from "flowbite-react";
+import { Navbar, Dropdown, Avatar } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { LuSearch, LuUser2 } from "react-icons/lu";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="p-5 md:px-6 md:py-8 lg:px-8 lg:py-10 border-black border-b bg-background">
       <Link to="/">
@@ -18,9 +20,34 @@ export default function Header() {
           </Link>
         </div>
         <div className="w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 border border-black flex items-center justify-center">
-          <Link to="/login">
-            <LuUser2 size={20} />
-          </Link>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar
+                  alt="user"
+                  img={currentUser.avatar && currentUser.avatar}
+                  rounded
+                />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm font-semibold">
+                  {currentUser.name}
+                </span>
+              </Dropdown.Header>
+              <Link to="/dashboard?tab=profile">
+                <Dropdown.Item>회원정보</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item>로그아웃</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to="/login">
+              <LuUser2 size={20} />
+            </Link>
+          )}
         </div>
       </div>
     </Navbar>
