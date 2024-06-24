0610c59066d753fcf51b6bb679311870f8fc7d31
so-zero <zero_s0@naver.com>
2024-06-24 16:56:31 +0900

    create post 기능 추가

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,22 +1,70 @@
-import React from "react";
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import axios from "axios";
 
 export default function CreatePost() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  const [thumbnail, setThumbnail] = useState("");
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Uncategorized");
+  const [content, setContent] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const postData = new FormData();
+    postData.set("title", title);
+    postData.set("category", category);
+    postData.set("content", content);
+    postData.set("thumbnail", thumbnail);
+
+    const URL = `${import.meta.env.VITE_BACKEND_URL}/post/create`;
+
+    try {
+      const response = await axios.post(URL, postData, {
+        withCredentials: true,
+        headers: { Authorization: `Bearer ${currentUser.token}` },
+      });
+
+      if (response.status === 201) {
+        return navigate(`/post/${response.data.slug}`);
+      }
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
   return (
     <section className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">News 작성하기</h1>
-      <form className="flex flex-col gap-4">
+      {error && (
+        <Alert className="mt-10 mx-10" color="failure">
+          {error}
+        </Alert>
+      )}
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
             type="text"
             id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             placeholder="제목"
             required
             className="flex-1"
           />
-          <Select>
+          <Select
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="uncategorized">✨카테고리</option>
             <option value="economy">💰경제</option>
             <option value="tech">🤖기술</option>
@@ -25,13 +73,21 @@ export default function CreatePost() {
             <option value="sport">⚽운동</option>
           </Select>
         </div>
-        <div className="flex gap-4 items-center justify-between border border-gray-300 p-2 rounded-lg">
-          <FileInput type="file" accept="png, jpg, jpeg" required />
-          <Button type="button" size="sm" color="dark">
-            업로드
-          </Button>
+        <div>
+          <FileInput
+            type="file"
+            accept="png, jpg, jpeg"
+            required
+            onChange={(e) => setThumbnail(e.target.files[0])}
+          />
         </div>
-        <ReactQuill theme="snow" className="h-96 mb-12" required />
+        <ReactQuill
+          theme="snow"
+          className="h-96 mb-12"
+          required
+          value={content}
+          onChange={setContent}
+        />
         <Button type="submit" size="lg" color="dark">
           등록하기
         </Button>
