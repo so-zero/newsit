14ffd9987ee1bb264f2fc68b5a19f6d618fdba05
so-zero <zero_s0@naver.com>
2024-06-26 23:04:45 +0900

    scroll top 기능 구현

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
