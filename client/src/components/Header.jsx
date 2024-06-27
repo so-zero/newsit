6d7ea2c8af449700f347d3d9afe97313deb3b855
so-zero <zero_s0@naver.com>
2024-06-27 21:00:03 +0900

    header style 수정

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Navbar, Dropdown, Avatar } from "flowbite-react";
+import React from "react";
+import { Navbar, Dropdown } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { LuSearch, LuUser2 } from "react-icons/lu";
 import { useSelector } from "react-redux";
 import { persistor } from "../redux/store";
-import axios from "axios";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const [avatar, setAvatar] = useState("");
-
-  useEffect(() => {
-    if (currentUser) {
-      const getAvatar = async () => {
-        const URL = `${import.meta.env.VITE_BACKEND_URL}/user/${
-          currentUser.id
-        }`;
-        const response = await axios.get(URL, {
-          withCredentials: true,
-          headers: { Authorization: `Bearer ${currentUser.token}` },
-        });
-        const { avatar } = response.data;
-        setAvatar(avatar);
-      };
-      getAvatar();
-    }
-  }, [currentUser]);
 
   const purge = async () => {
     window.location.reload();
@@ -40,24 +21,14 @@ export default function Header() {
         </h1>
       </Link>
       <div className="flex items-center cursor-pointer">
-        <div className="w-12 h-12 lg:w-14 lg:h-14 border border-black flex items-center justify-center">
+        <div className="w-12 h-12 lg:w-14 lg:h-14 border border-black flex items-center justify-center border-r-0">
           <Link to="/search">
             <LuSearch size={20} />
           </Link>
         </div>
         <div className="w-12 h-12 lg:w-14 lg:h-14 border border-black flex items-center justify-center">
           {currentUser ? (
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={
-                <Avatar
-                  alt="user"
-                  img={`${import.meta.env.VITE_ASSETS_URL}/uploads/${avatar}`}
-                  rounded
-                />
-              }
-            >
+            <Dropdown arrowIcon={false} inline label={<LuUser2 size={20} />}>
               <Dropdown.Header>
                 <span className="block text-sm font-semibold">
                   {currentUser.name}
